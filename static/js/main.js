12c28850b3b5ad2cd42f6dcbdb13c46dcee625d6
HugoAndres23 <71717006+HugoAndres23@users.noreply.github.com>
2024-11-20 15:15:54 -0500

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -8,18 +8,14 @@ const posY = document.getElementById('pos-y');
 const tiempo = document.getElementById('tiempo');
 const radio = document.getElementById('radio');
 
-// Validación de inputs
 posX.addEventListener('input', () => validateInput(posX));
 posY.addEventListener('input', () => validateInput(posY));
 
-// Variable para almacenar el intervalo
 let intervalId;
 
-// Evento de inicio (start)
 start.addEventListener("click", async (event) => {
   event.preventDefault();
 
-  // Iniciar el intervalo para las peticiones
   intervalId = setInterval(async () => {
     const response = await fetch("/aplicar_soplete", {
       method: "POST",
@@ -41,25 +37,21 @@ start.addEventListener("click", async (event) => {
     } else {
       console.error("Error al actualizar la imagen de la placa.");
     }
-  }, 1000); // Realiza la petición cada 1 segundo
+  }, 1000);
 });
 
-// Evento de parada (stop)
 stop.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // Detener el intervalo cuando se haga clic en el botón "stop"
   if (intervalId) {
     clearInterval(intervalId);
   }
 });
 
-// Función para actualizar el valor de la temperatura
 function updateTempValue(value) {
   temp_value.textContent = value + '°C';
 }
 
-// Función de validación para los inputs
 function validateInput(input) {
   const min = parseInt(input.min);
   const max = parseInt(input.max);
